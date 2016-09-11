b5d322250bcff8fcd0dbbb3357ac517887d32d56
brobeson <brobeson@users.noreply.github.com>
2016-09-10 22:38:35 -0600

    cs 6680 - #1 - finish the second problem of the class exercises

--- cs_6680_computer_vision/exercises/class_2016_09_08.m
+++ cs_6680_computer_vision/exercises/class_2016_09_08.m
@@ -49,6 +49,65 @@ pause
 %}}}
 
 %% Problem 2 {{{
+% Write down mathematical formulas for all the 6 solid black curves on slide #9
+% of class lecture Ch3.1.DIPBasicSpatial.pdf.
+%
+% The curves are: negative, log, nth root, identity, nth power, and inverse log
+%
+% negative:    f(l) = L - 1 - l for l on [0, L - 1]
+% log:         f(l) = l^0.2     for l on [0, 1]
+% nth root:    f(l) = sqrt(l)   for l on [0, 1]
+% identity:    f(l) = l         for l on [0, L - 1]
+% nth power:   f(l) = l^2       for l on [0, 1]
+% inverse log: f(l) = l^5       for l on [0, 1]
+
+% define the X range. the power functions only work on [0, 1], so that's the
+% range used.
+x0 = 0.0;
+xN = 1.0;
+steps = 100; % divide the range into 100 steps to get smooth curves
+x = [x0: (xN - x0) / steps: xN];
+
+% the six curves detailed above
+y_negative = 1.0 - x;
+y_log = x .^ 0.2;
+y_nth_root = x .^ 0.5;
+y_identity = x;
+y_nth_power = x .^ 2;
+y_inverse_log = x .^ 5;
+
+% draw the curves
+figure
+y = 0.75;
+plot(x, y_negative,
+     x, y_log,
+     x, y_nth_root,
+     x, y_identity,
+     x, y_nth_power,
+     x, y_inverse_log);
+xlabel('Input gray level, r');
+ylabel('Output gray level, s');
+title('Intensity Transformations');
+legend('s(r) = L - r',
+       's(r) = x^{0.2}',
+       's(r) = x^{0.5}',
+       's(r) = r',
+       's(r) = x^2',
+       's(r) = x^5');
+axis square;
+ax = gca;
+set(ax, 'XTick', [x0: 0.1: xN]);
+set(ax, 'YTick', [x0: 0.1: xN]);
+%ax.XTick = [x0:0.1:xN];
+%ax.YTick = [x0:0.1:xN];
+
+% draw the same lines visible in the notes
+line([x0 xN], [0.5 0.5]);
+line([x0 xN], [0.75 0.75]);
+line([0.5 0.5], [0 1]);
+line([0.25 0.25], [0 1]);
+
+pause
 
 %}}}
 
