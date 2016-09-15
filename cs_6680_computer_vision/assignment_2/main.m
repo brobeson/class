034f062d04958d76a066cad47f8addaa4374fcec
brobeson <brobeson@users.noreply.github.com>
2016-09-14 21:58:17 -0600

    cs 6680 - #3 - preliminary work on assignment 2 problem 2

--- cs_6680_computer_vision/assignment_2/main.m
+++ cs_6680_computer_vision/assignment_2/main.m
@@ -5,7 +5,7 @@
 food = imread('Food.jpg');
 
 % Problem 1 {{{
-[scaled_image scale_function] = Scaling(food, [0, 255]);
+[scaled_food scale_function] = Scaling(food, [0, 255]);
 r = [min(min(food)):max(max(food))];
 figure 1;
 plot(r, scale_function);
@@ -15,6 +15,25 @@ ylabel('ScaledIm image intensity, f(i)');
 title('Linear Scaling of an Image');
 
 disp('-----Finish Solving Problem 1-----')
+pause
+% }}}
+
+% Problem 2 {{{
+%matScaledFood = imadjust(food, [0; 255]) .* 255;
+matScaledFood = imadjust(food, [min(min(food)); max(max(food))], [0; 255]);
+%class(matScaledFood)
+%min(min(matScaledFood))
+%max(max(matScaledFood))
+figure 2;
+subplot(1, 2, 1);
+imshow(scaled_food);
+title('My scaled food');
+
+subplot(1, 2, 2);
+imshow(matScaledFood);
+title('Matlab scaled food');
+
+disp('-----Finish Solving Problem 2-----')
 pause
 % }}}
 
