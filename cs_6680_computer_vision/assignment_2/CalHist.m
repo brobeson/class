6ab1cdb23c5ada94fcd4282654063c1bd96a2fcc
brobeson <brobeson@users.noreply.github.com>
2016-09-17 21:51:51 -0600

    cs 6680 - #3 - add test results for CalHist running Octave
    This is for two different methods of calculating the histogram. I need to test
    both methods in Matlab for completeness.

--- cs_6680_computer_vision/assignment_2/CalHist.m
+++ cs_6680_computer_vision/assignment_2/CalHist.m
@@ -14,11 +14,28 @@ function [histogram, norm_histogram] = CalHist(img, normalized)
         end
     end
 
-    histogram(1:256) = 0;
+    histogram = zeros(1, 256);
+
+    % loop through the image. increment the histogram bin corresponding to the
+    % intensity. remember that intensity on [ 0, 255], but Matlab requires
+    % indexing on [1, 256]. so ad 1 to the histogram index.
+    %tic
+    %lngth = size(img, 1) * size(img, 2);
+    %for i = 1:lngth
+    %    histogram(img(i) + 1) = histogram(img(i) + 1) + 1;
+    %end
+    %toc
+
+    % this algorithm wins in Octave
+    tic
     for i = [0:255]
         histogram(i + 1) = sum(sum(img == i));
     end
+    toc
 
+    % if the user requested ONLY the normalized histogram, give it to them.
+    % otherwise, if the user requested both the histogram and the normalized
+    % histogram, give them both.
     if (nargout ~= 2) && (nargin == 2) && normalized
         [rows, cols] = size(img);
         histogram = double(histogram) ./ (rows * cols);
