e45b8664eeff06cb5a837af38cbfe0bb24f52cd9
brobeson <brobeson@users.noreply.github.com>
2016-09-29 23:11:28 -0600

    cs 6680 - #4 - work on assignment 3 problem I.2

---
+++ cs_6680_computer_vision/assignment_3/MedianFiltering.m
@@ -0,0 +1,50 @@
+% Brendan Robeson
+% CS 6680
+% Assignment 3
+
+function [filteredIm] = MedianFiltering(im, mask)
+%   AVERAGEFILTERING    Filter an image by averaging using the supplied filter mask.
+%
+%       im      The input image. This should be of class uint8.
+%       mask    The filter mask. This must be a square mask, with an odd number
+%               of rows, and symmetric about the center.
+%
+%       filteredIm  The output image. This will be of class uint8
+
+% verify the input
+assert(isa(im, 'uint8') ~= 0, 'input image must be of type uint8, not %s', class(im));
+assert(issquare(mask), 'the filter mask must be square');
+assert(mod(size(mask, 1), 2) ~= 0, 'the filter mask must have an odd number of rows');
+
+% compute the amount by which to pad the image. also compute the start and end
+% indices for the actual image rows and columns within the padded image
+pad_amount = floor(size(mask, 1) / 2);
+image_size = size(im);
+r0 = 1 + pad_amount;
+rN = image_size(1) + pad_amount;
+c0 = r0;
+cN = image_size(2) + pad_amount;
+
+% create a padded image of 0s, then fill the center area with the source image
+padded = zeros(image_size + (2 * pad_amount), class(im));
+padded(r0:rN, c0:cN) = im(1:image_size(1), 1:image_size(2));
+
+% apply the average filter, using the supplied mask
+filteredIm = zeros(image_size, 'uint8');
+for r = 1:image_size(1)
+    % r0 is the initial row in the padded image, rN is the last row in the
+    % padded image FOR APPLICATION OF THE MASK. thus,
+    % rN - r0 + 1 = size(mask, 1). accessing the padded image, and destination
+    % image, use the same r0: writing to filteredIm row 1 requires reading from
+    % padded at rows 1 through the mask height. the same concept applies to the
+    % columns.
+    r0 = r;
+    rN = r0 + 2 * pad_amount;
+    for c = 1:image_size(2)
+        c0 = c;
+        cN = c0 + 2 * pad_amount;
+        filteredIm(r, c) = median(padded(r0:rN, c0:cN)(:));
+    end
+end
+
+end
--- cs_6680_computer_vision/assignment_3/main.m
+++ cs_6680_computer_vision/assignment_3/main.m
@@ -16,7 +16,7 @@ standard_mask = ones(5, 5);
 standard_mask = standard_mask ./ sum(standard_mask(:));
 standard_image = AverageFiltering(circuit, standard_mask);
 
-% plot the scale_function as a function of the intensties in 'food'
+% show the circuit images
 figure(1);
 subplot(1, 3, 1);
 imshow(circuit);
@@ -29,6 +29,31 @@ imshow(standard_image);
 title('5x5 Standard average');
 
 disp('-----Finish Solving Problem I.1-----')
+drawnow; % work around Matlab R2016a bug that can cause 'pause' to hang
+pause
+% }}}
+
+%% Part 2 {{{
+%weighted_mask = [1 2 1; 2 4 2; 1 2 1];
+%weighted_mask = weighted_mask ./ sum(weighted_mask(:));
+%weighted_image = AverageFiltering(circuit, weighted_mask);
+
+standard_mask = ones(5, 5);
+standard_image = MedianFiltering(circuit, standard_mask);
+
+% show the circuit images
+figure(2);
+subplot(1, 3, 1);
+imshow(circuit);
+title('Original circuit.jpg');
+%subplot(1, 3, 2);
+%imshow(weighted_image);
+%title('3x3 Weighted average');
+subplot(1, 3, 3);
+imshow(standard_image);
+title('5x5 Standard median');
+
+disp('-----Finish Solving Problem I.2-----')
 drawnow; % work around Matlab R2016a bug that can cause 'pause' to hang
 pause
 % }}}
