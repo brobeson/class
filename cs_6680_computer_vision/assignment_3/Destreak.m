b38beeb48bb3a565a33f3058460cad257a1d0d12
brobeson <brobeson@users.noreply.github.com>
2016-10-02 23:03:44 -0600

    cs 6680 - #4 - much better solution for assignment 3, problem III

--- cs_6680_computer_vision/assignment_3/Destreak.m
+++ cs_6680_computer_vision/assignment_3/Destreak.m
@@ -7,12 +7,15 @@ function output_image = Destreak(image)
     negated = 1.0 - normalized;
     med = medfilt2(negated, [7 7]);
     diff = negated - med;
-    %med2 = medfilt2(diff, [5 5]);
-    %diff2 = diff - med2;
-    output_image = 1.0 - diff;
+    avg = imfilter(diff, [1 2 1; 2 4 2; 1 2 1]);
+    m = min(avg(:));
+    M = max(avg(:));
+    avg = (avg - m) / (M - m);
+    thresholded = double(avg > 0.5 * max(avg(:)));
+    output_image = 1.0 - thresholded;
 
     figure;
-    rows = 2;
+    rows = 3;
     cols = 3;
     subplot(rows, cols, 1);
     imshow(image);
@@ -26,13 +29,13 @@ function output_image = Destreak(image)
     subplot(rows, cols, 4);
     imshow(diff);
     title('Difference image');
-    %subplot(rows, cols, 5);
-    %imshow(med2);
-    %title('2nd median image');
-    %subplot(rows, cols, 6);
-    %imshow(diff2);
-    %title('2nd difference image');
+    subplot(rows, cols, 5);
+    imshow(avg);
+    title('Average');
     subplot(rows, cols, 6);
+    imshow(thresholded);
+    title('Thresholded image');
+    subplot(rows, cols, 8);
     imshow(output_image);
     title('Output image');
 end
