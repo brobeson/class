87a9bb66e9fae69b32d7c25a4773cbda38368874
brobeson <brobeson@users.noreply.github.com>
2016-10-04 22:54:43 -0600

    cs 6680 - #4 - finish assignment 3, problem I.2

--- cs_6680_computer_vision/assignment_3/MedianFiltering.m
+++ cs_6680_computer_vision/assignment_3/MedianFiltering.m
@@ -30,12 +30,20 @@ cN = image_size(2) + pad_amount;
 padded = zeros(image_size + (2 * pad_amount), class(im));
 padded(r0:rN, c0:cN) = im(1:image_size(1), 1:image_size(2));
 
+% allocate a vector to hold the image values selected via the mask. we know that
+% the vector will have a number of elements equal to the sum of the mask values.
+% for example, the weighted mask [ 1 2 1; 2 4 2; 1 2 1] will extract 16 pixels
+% from the image
+vector_size = sum(mask(:));
+vec = zeros(1, vector_size);
+
 % apply the average filter, using the supplied mask
 filteredIm = zeros(image_size, 'uint8');
 for r = 1:image_size(1)
     for c = 1:image_size(2)
-        values = padded(r:r + mask_size - 1, c:c + mask_size - 1);
-        filteredIm(r, c) = median(values(:));
+        submatrix = padded(r:r + mask_size - 1, c:c + mask_size - 1);
+        v = submatrix(:);
+        filteredIm(r, c) = median(repelem(v, mask(:))');
     end
 end
 
--- cs_6680_computer_vision/assignment_3/main.m
+++ cs_6680_computer_vision/assignment_3/main.m
@@ -5,25 +5,23 @@
 clc
 pause off
 
-Destreak(imread('Text.gif'));
-Destreak(imread('Text1.gif'));
-%Destreak(imread('Text2.jpg'));
-return
+%Destreak(imread('Text.gif'));
+%Destreak(imread('Text1.gif'));
+%%Destreak(imread('Text2.jpg'));
+%return
 
 %% Problem I {{{
 circuit = imread('Circuit.jpg');
 %% Part 1 {{{
-weighted_mask = [1 2 1; 2 4 2; 1 2 1];
+weighted_mask = [ 1 2 1;
+                  2 4 2;
+                  1 2 1 ];
 weighted_mask = weighted_mask ./ sum(weighted_mask(:));
-tic
 weighted_circuit = AverageFiltering(circuit, weighted_mask);
-toc
 
 standard_mask = ones(5, 5);
 standard_mask = standard_mask ./ sum(standard_mask(:));
-tic
 standard_circuit = AverageFiltering(circuit, standard_mask);
-toc
 
 % show the circuit images
 figure(1);
@@ -43,23 +41,22 @@ pause
 % }}}
 
 %% Part 2 {{{
-%weighted_mask = [1 2 1; 2 4 2; 1 2 1];
-%weighted_mask = weighted_mask ./ sum(weighted_mask(:));
-%weighted_circuit = AverageFiltering(circuit, weighted_mask);
+weighted_mask = [ 1 2 1;
+                  2 4 2;
+                  1 2 1 ];
+weighted_circuit = MedianFiltering(circuit, weighted_mask);
 
 standard_mask = ones(5, 5);
-tic
 standard_circuit = MedianFiltering(circuit, standard_mask);
-toc
 
 % show the circuit images
 figure(2);
 subplot(1, 3, 1);
 imshow(circuit);
 title('Original circuit.jpg');
-%subplot(1, 3, 2);
-%imshow(weighted_circuit);
-%title('3x3 Weighted average');
+subplot(1, 3, 2);
+imshow(weighted_circuit);
+title('3x3 Weighted median');
 subplot(1, 3, 3);
 imshow(standard_circuit);
 title('5x5 Standard median');
