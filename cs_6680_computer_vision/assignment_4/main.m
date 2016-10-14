7f1d293c357f93a3fc34ac03a5e65fea08becc23
brobeson <brobeson@users.noreply.github.com>
2016-10-14 11:25:37 -0600

    cs 6680 - #5 - finish assignment 4 problem I.1

--- cs_6680_computer_vision/assignment_4/main.m
+++ cs_6680_computer_vision/assignment_4/main.m
@@ -5,6 +5,44 @@
 clc
 pause off
 
+%% Problem II - Noise modeling {{{
+city = imread('City.jpg');
+
+%% Part 1 - Blurring {{{
+k = 0.0025;
+
+% build the filter according to the formula
+%   H(u,v) = e^(-k D(u,v)^5/3)
+sz = size(city);
+rws = repmat([1:sz(1)]',     1, sz(2)); % a matrix of row numbers
+cls = repmat([1:sz(2)],  sz(1),     1); % a matrix of column numbers
+center = [ floor(sz(1) / 2 + 1), floor(sz(2) / 2 + 1) ];
+filter = exp(-k .* ((rws - center(1)).^2 + (cls - center(2)).^2) .^ (5/6));
+
+% apply the filter in the frequency domain, then convert back to spatial
+blur_city = ifft2(ifftshift(fftshift(fft2(city)) .* filter));
+
+% show the images
+figure(1);
+subplot(1, 3, 1);
+imshow(city);
+title('Original city.jpg');
+subplot(1, 3, 2);
+imshow(filter, []);
+title('Filter H');
+subplot(1, 3, 3);
+imshow(blur_city, []);
+title('Blurred city');
+
+disp('-----Finish Solving Problem II part 1-----')
+drawnow; % work around Matlab R2016a bug that can cause 'pause' to hang
+pause
+% }}}
+
+% }}}
+
+return
+
 %% Problem I - FFT filters {{{
 sample = imread('Sample.jpg');
 
