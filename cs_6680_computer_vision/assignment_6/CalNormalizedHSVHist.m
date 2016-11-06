f6b05c58b53835258badc92481431610842fab76
brobeson <brobeson@users.noreply.github.com>
2016-11-06 14:15:25 -0700

    cs 6680 - #7 - fix assignment 6 problem 2 part 1
    I had the histogram bins arranged incorrectly. I wasn't binning the permutations
    of hue, saturation, and value.

--- cs_6680_computer_vision/assignment_6/CalNormalizedHSVHist.m
+++ cs_6680_computer_vision/assignment_6/CalNormalizedHSVHist.m
@@ -1,5 +1,5 @@
 function Hist = CalNormalizedHSVHist(Im, HbinNum, SbinNum, VbinNum)
-Hist = zeros(1, HbinNum * SbinNum * VbinNum, 'uint32');
+Hist = zeros(1, HbinNum * SbinNum * VbinNum, 'double');
 
 % convert the RGB image to HSV
 hsv = rgb2hsv(Im);
@@ -9,26 +9,25 @@ ranges = [ 1.0 / HbinNum, 1.0 / SbinNum, 1.0 / VbinNum ];
 
 % calculate the bin to which each value belongs
 bins = zeros(size(hsv), 'uint32');
-bins(:,:,1) = hsv(:,:,1) ./ ranges(1);
-bins(:,:,2) = hsv(:,:,2) ./ ranges(2);
-bins(:,:,3) = hsv(:,:,3) ./ ranges(3);
+bins(:,:,1) = uint32(ceil(hsv(:,:,1) ./ ranges(1)));
+bins(:,:,2) = uint32(ceil(hsv(:,:,2) ./ ranges(2)));
+bins(:,:,3) = uint32(ceil(hsv(:,:,3) ./ ranges(3)));
 
-% fill in the hue portion of the histogram
+bin = 1;
 for h = 1:HbinNum
-    Hist(h) = sum(sum(bins(:,:,1) == h));
-end
-
-% fill in the saturation portion of the histogram
-for s = 1:SbinNum
-    Hist(HbinNum + s) = sum(sum(bins(:,:,2) == s));
-end
-
-% fill in the value portion of the histogram
-for v = 1:VbinNum
-    Hist(HbinNum + SbinNum + v) = sum(sum(bins(:,:,3) == v));
+    hue = bins(:,:,1) == h;
+    for s = 1:SbinNum
+        saturation = bins(:,:,2) == s;
+        for v = 1:VbinNum
+            value = bins(:,:,3) == v;
+            permutation = value & saturation & hue;
+            Hist(bin) = sum(sum(permutation));
+            bin = bin + 1;
+        end
+    end
 end
 
 % convert from histogram to normalized histogram
-Hist = Hist ./ size(hsv, 1) * size(hsv, 2);
+Hist = Hist ./ (size(hsv, 1) * size(hsv, 2));
 
 end
