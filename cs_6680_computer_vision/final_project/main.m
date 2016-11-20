a2bc1f9deedc5d9e259fc14dba9f9223edc87dfd
brobeson <brobeson@users.noreply.github.com>
2016-11-19 23:37:47 -0700

    cs 6680 - #10 - finish the uav_sift() function
    The function now adds the additional color information to the descriptor as
    described in the paper.

--- cs_6680_computer_vision/final_project/main.m
+++ cs_6680_computer_vision/final_project/main.m
@@ -1,6 +1,14 @@
-%img = imread('~/Documents/class/cs6680/final_project/data_sets/horizontal_mapping_4cm/IMG_0078.JPG');
-%[f, d] = uav_sift(img);
+img = imread('~/Documents/class/cs6680/final_project/data_sets/horizontal_mapping_4cm/IMG_0078.JPG');
+%img = imread('lena.jpg');
+%img = cat(3, img, img, img);
 
-img = imread('lena.jpg');
-img = cat(3, img, img, img);
+tic
 [f, d] = uav_sift(img);
+end_time = toc;
+fprintf(1, 'uav_sift() ran in %.2f seconds\n', end_time);
+
+figure;
+imshow(img);
+hold on
+plot(f(1,:), f(2,:), 'r.');
+hold off
--- cs_6680_computer_vision/final_project/uav_sift.m
+++ cs_6680_computer_vision/final_project/uav_sift.m
@@ -3,35 +3,49 @@ function [frames, descriptors] = uav_sift(img)
     % TODO  make this more robust. i know my test data is uint8 RGB. this
     %       function should appropriately handle grayscale and floating point
     %       data.
-    assert(ndims(img) == 3, 'at this time, uav_sift() only supports RGB images');
-
-    % the image is needed in three color spaces: rgb, hsv, and grayscale
-    %if ndims(img) == 3 % input is rgb
-    %    img_rgb = img;
-    %    img_hsv = 
-
-    % normal sift is only defined for grayscale, single precision, floating point
-    % data.
-    %if ndims(img) == 3
-    %    img = rgb2gray(img);
-    %end
-    %if isa(img, 'integer')
-    %    img = im2double(img);
-    %end
-    %if isa(img, 'double')
-    %    img = single(img);
-    %end
+    assert(ndims(img) == 3,   'at this time, uav_sift() only supports RGB images');
+    assert(isa(img, 'uint8'), 'at this time, uav_sift() only supports uint8 image data');
 
     % get the basic SIFT frames (keypoints) and their descriptors
     %[frames, descriptors] = vl_sift(img);
     [frames, descriptors] = vl_sift(im2single(rgb2gray(img)));
 
+    % HSV information will be appended to the basic descriptor, so convert the
+    % image. also, hue is typically described on [0,1] or [1,360], neither of
+    % which can be represented by a uint8. so convert the entire descriptor to
+    % single.
+    img_hsv = rgb2hsv(img);
+    descriptors = single(descriptors);
+
+    % cascaded image dilations & erosions
+    img_d1 = imdilate(img,    strel('disk', 3));
+    img_d2 = imdilate(img_d1, strel('disk', 4));
+    img_d3 = imdilate(img_d2, strel('disk', 5));
+    img_e1 = imerode(img,    strel('disk', 3));
+    img_e2 = imerode(img_e1, strel('disk', 4));
+    img_e3 = imerode(img_e2, strel('disk', 5));
+
     % expand the descriptors to make room for the color information as described
     % in [1]. then fill in the data.
     descriptors(129:152, 1) = 0;
-    %for d = 1:size(descriptors, 2)
-    %    % get the RGB color. the frame has fractional coordinates; for now,
-    %    % round to get the pixel coordinates
-    %    descriptors(129:
-    %end
+    for d = 1:size(descriptors, 2)
+        % get the RGB color. the frame has fractional coordinates; for now,
+        % round to get the pixel coordinates
+        x = round(frames(1, d));
+        y = round(frames(2, d));
+        descriptors(129:131, d) = img(y, x, :);
+
+        % get the HSV of the 
+        descriptors(132:134, d) = img_hsv(y, x, :);
+
+        % the 3 dilations
+        descriptors(135:137, d) = img_d1(y, x, :);
+        descriptors(138:140, d) = img_d2(y, x, :);
+        descriptors(141:143, d) = img_d3(y, x, :);
+
+        % the 3 erosions
+        descriptors(144:146, d) = img_e1(y, x, :);
+        descriptors(147:149, d) = img_e2(y, x, :);
+        descriptors(150:152, d) = img_e3(y, x, :);
+    end
 end
