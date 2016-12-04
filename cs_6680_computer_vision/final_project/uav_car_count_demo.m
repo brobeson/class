72af4b9c0383cc3c7b88e8b7b460a334c0162518
brobeson <brobeson@users.noreply.github.com>
2016-12-04 07:54:25 -0700

    cs 6680 - #10 - make the uav_car_counter function return a matrix of times
    Each row corresponds to an image. Each column is a major step in the process.
    The last column is the total time.

--- cs_6680_computer_vision/final_project/uav_car_count_demo.m
+++ cs_6680_computer_vision/final_project/uav_car_count_demo.m
@@ -1,3 +1,7 @@
+clc
+clear all
+close all
+
 % load the truth data and extract the column of true car counts
 truth_data = readtable('true_counts_small.txt', 'ReadRowNames', true);
 truth_data = sortrows(truth_data, 'RowNames');
@@ -9,12 +13,26 @@ image_files = { 'small_img2.png' 'small_img.png' };
 svm_files = { 'uav_asphalt_svm.mat' 'uav_keypoint_svm.mat' };
 [counts, run_times] = uav_car_counter(image_files, svm_files);
 
+% separate the run times into distinct variables. not sure why, but assigning a
+% matrix to the table doesn't work right.
+asphalt_time = run_times(:, 1);
+key_point_time = run_times(:, 2);
+classify_time = run_times(:, 3);
+removal_time = run_times(:, 4);
+merge_time = run_times(:, 5);
+total_time = run_times(:, 6);
+
 % put the data into a table
-data_table = table(counts, run_times);
-data_table.Properties.RowNames = image_files;
-data_table = sortrows(data_table, 'RowNames');
-data_table.true_counts = true_counts;
-data_table.accuracy = (data_table.counts ./ data_table.true_counts) .* 100;
-data_table = data_table(:, [1 3 4 2]);
-data_table
-writetable(data_table, 'demo.txt', 'WriteRowNames', true);
+count_table = table(counts);
+count_table.Properties.RowNames = image_files;
+count_table = sortrows(count_table, 'RowNames');
+count_table.true_counts = true_counts;
+count_table.accuracy = (count_table.counts ./ count_table.true_counts) .* 100;
+count_table
+writetable(count_table, 'demo_counts.txt', 'WriteRowNames', true);
+
+times_table = table(asphalt_time, key_point_time, classify_time, removal_time, merge_time, total_time);
+times_table.Properties.RowNames = image_files;
+times_table = sortrows(times_table, 'RowNames');
+times_table
+writetable(times_table, 'demo_times.txt', 'WriteRowNames', true);
--- cs_6680_computer_vision/final_project/uav_car_counter.m
+++ cs_6680_computer_vision/final_project/uav_car_counter.m
@@ -11,9 +11,16 @@ function [N, run_times] = uav_car_counter(img_files, svm_files) % {{{
     %               the key point classification SVM file.
     %   N           A vector of car counts. Each element is the car count for
     %               the corresponding element in img_files.
-    %   run_times   A vector of run times. Each element is the total run time
-    %               for the corresponding element in img_files. The run times
-    %               are measured in seconds.
+    %   run_times   A matrix of run times. Each row is a set of run times for
+    %               the corresponding element in img_files. Each column
+    %               corresponds to the following major steps:
+    %                   Asphalt segmentation
+    %                   Key point extraction
+    %                   Key point classification
+    %                   Key point reduction
+    %                   Key point merging
+    %                   Total run time
+    %               The run times are measured in seconds.
     %
     %   This function will run each image in img_files through the UAV car
     %   counting algorithm. It will return various data about the algorithm
@@ -22,34 +29,32 @@ function [N, run_times] = uav_car_counter(img_files, svm_files) % {{{
     assert(nargin == 2, 'two input arguments are required');
     assert(length(svm_files) >= 2, 'svm_files must have at least two elements');
 
-    % load the asphalt SVM {{{
+    % load the asphalt SVM
     svm_struct = load(char(svm_files(1)));
     asphalt_svm = svm_struct.svm;
     assert(isa(asphalt_svm, 'ClassificationSVM'), 'loaded asphalt SVM is not really an SVM');
-    % }}}
 
-    % load the key point classification SVM {{{
+    % load the key point classification SVM
     svm_struct = load(char(svm_files(2)));
     keypoint_svm = svm_struct.svm;
     assert(isa(keypoint_svm, 'ClassificationSVM'), 'loaded key point SVM is not really an SVM');
-    % }}}
 
     % form the output variables
-    run_times = zeros(length(img_files), 1);
+    run_times = zeros(length(img_files), 6);
     N = zeros(length(img_files), 1);
 
-    % load the image to count {{{
+    % load the image to count
     for f = 1:length(img_files)
-        [N(f), run_times(f)] = uav_count_cars(imread(char(img_files(f))), asphalt_svm, keypoint_svm, 0);
+        [N(f), run_times(f, :)] = uav_count_cars(imread(char(img_files(f))), asphalt_svm, keypoint_svm, 0);
     end
-
-    % }}}
 end
 % }}}
 
 function [n, t] = uav_count_cars(img, asphalt_svm, keypoint_svm, verbose) % {{{
     fig = 1; % semi-auto incrementing figure number
-    tic
+    t = zeros(1, 6);
+
+    t_start = tic;
     % extract the asphalt segments {{{
     if verbose ~= 0
         log_message('extracting asphalt');
@@ -95,7 +100,9 @@ function [n, t] = uav_count_cars(img, asphalt_svm, keypoint_svm, verbose) % {{{
     end
     % }}}
     % }}}
+    t(1) = toc(t_start);
 
+    t_intermediate = tic;
     % extract key points from the test image {{{
     if verbose ~= 0
         log_message('extracting key points. this typically takes around 30 seconds');
@@ -115,7 +122,9 @@ function [n, t] = uav_count_cars(img, asphalt_svm, keypoint_svm, verbose) % {{{
     end
     % }}}
     % }}}
+    t(2) = toc(t_intermediate);
 
+    t_intermediate = tic;
     % classify the key points {{{
     if verbose ~= 0
         log_message('classifying the key points');
@@ -148,7 +157,9 @@ function [n, t] = uav_count_cars(img, asphalt_svm, keypoint_svm, verbose) % {{{
     end
     % }}}
     % }}}
+    t(3) = toc(t_intermediate);
 
+    t_intermediate = tic;
     % remove key points which are not in asphalt {{{
     if verbose ~= 0
         log_message('removing key points from non-asphalt areas');
@@ -177,7 +188,9 @@ function [n, t] = uav_count_cars(img, asphalt_svm, keypoint_svm, verbose) % {{{
     end
     % }}}
     % }}}
+    t(4) = toc(t_intermediate);
 
+    t_intermediate = tic;
     % merge the key points {{{
     if verbose ~= 0
         log_message('merging key points');
@@ -200,6 +213,7 @@ function [n, t] = uav_count_cars(img, asphalt_svm, keypoint_svm, verbose) % {{{
     end
     % }}}
     % }}}
-    t = toc;
+    t(5) = toc(t_intermediate);
+    t(6) = toc(t_start);
 end
 % }}}
