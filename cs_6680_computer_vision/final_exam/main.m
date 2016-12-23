9418cb1f303e89b12de4f3bc6977925b89877205
brobeson <brobeson@users.noreply.github.com>
2016-12-23 14:24:00 -0700

    final commit for cs 6680

--- cs_6680_computer_vision/final_exam/main.m
+++ cs_6680_computer_vision/final_exam/main.m
@@ -1,27 +1,32 @@
-% problem 3.1 {{{
-x = [ 0 0 5 5 4 1 ];
-y = [ 0 1 4 5 5 0 ];
-
-figure;
-plot(x, y, '.r');
-% }}}
-
-return
-
 % problem 1.3 {{{
 original_img = imread('Wirebond.tif');
-figure;
+figure(1);
 subplot(1, 3, 1);
 imshow(original_img);
 
 %img = imopen(original_img, strel('square', 3));
 %img = imopen(img, strel('square', 5));
 img = imopen(original_img, strel('disk', 8));
+%img = imopen(img, strel('rectangle', [10 5]));
+
+%img = imerode(img, strel('rectangle', [10 5]));
+
 subplot(1, 3, 2);
 imshow(img);
 
+
 subplot(1, 3, 3);
 imshow(abs(original_img - img));
+% }}}
+
+return
+
+% problem 3.1 {{{
+x = [ 0 0 5 5 4 1 ];
+y = [ 0 1 4 5 5 0 ];
+
+figure;
+plot(x, y, '.r');
 % }}}
 
 return
