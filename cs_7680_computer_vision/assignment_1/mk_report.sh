5e52113d1928e33a529e2d5bad1ee3db29956e79
brobeson <brobeson@users.noreply.github.com>
2017-01-15 19:01:04 -0700

    cs 7680 - #13 - finish assignment 1

--- cs_7680_computer_vision/assignment_1/mk_report.sh
+++ cs_7680_computer_vision/assignment_1/mk_report.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-pdflatex -file-line-error -halt-on-error "$1"
+pdflatex -file-line-error -halt-on-error "$1.tex" && \
+bibtex "$1.aux" && \
+pdflatex -file-line-error -halt-on-error "$1.tex" && \
+pdflatex -file-line-error -halt-on-error "$1.tex"
