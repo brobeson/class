2c877e7e797c409929615dad30c0cc1135439367
brobeson <brobeson@users.noreply.github.com>
2017-01-20 13:46:18 -0700

    cs 7680 - #14 - progress on assignment 1

--- cs_7680_computer_vision/mk_report.sh
+++ cs_7680_computer_vision/mk_report.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1.tex" && \
+#pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1" && \
+#bibtex "$1.aux" && \
+#pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1" && \
+#pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1"
+
+pdflatex -file-line-error -halt-on-error "$1.tex" && \
 bibtex "$1.aux" && \
-pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1.tex" && \
-pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1.tex"
+pdflatex -file-line-error -halt-on-error "$1.tex" && \
+pdflatex -file-line-error -halt-on-error "$1.tex"
