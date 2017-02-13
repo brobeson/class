39976023acfd33eebc250295e650880c79e13c58
brobeson <brobeson@users.noreply.github.com>
2017-02-12 23:40:07 -0700

    cs 7680 - #16 - work on assignment 3

--- cs_7680_computer_vision/mk_report.sh
+++ cs_7680_computer_vision/mk_report.sh
@@ -5,7 +5,7 @@
 #pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1" && \
 #pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1"
 
-pdflatex -file-line-error -halt-on-error "$1.tex" && \
-bibtex "$1.aux" && \
-pdflatex -file-line-error -halt-on-error "$1.tex" && \
 pdflatex -file-line-error -halt-on-error "$1.tex"
+#bibtex "$1.aux" && \
+#pdflatex -file-line-error -halt-on-error "$1.tex" && \
+#pdflatex -file-line-error -halt-on-error "$1.tex"
