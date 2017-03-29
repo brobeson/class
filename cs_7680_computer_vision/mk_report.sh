70009e77e0bd4be9760e2c155374a202414e073d
brobeson <brobeson@users.noreply.github.com>
2017-03-29 12:17:31 -0600

    cs 7680 - working on assignment 5

--- cs_7680_computer_vision/mk_report.sh
+++ cs_7680_computer_vision/mk_report.sh
@@ -5,7 +5,12 @@
 #pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1" && \
 #pdflatex -output-directory ~/Documents/phd/cs7680/ -file-line-error -halt-on-error "$1"
 
-pdflatex -file-line-error -halt-on-error "$1.tex"
+mkdir -p /tmp/latex/output
+pdflatex -output-directory /tmp/latex/output/ -file-line-error -halt-on-error "$1.tex"
+if [[ $? == 0 ]]
+then
+    cp /tmp/latex/output/$1.pdf ./
+fi
 #bibtex "$1.aux" && \
 #pdflatex -file-line-error -halt-on-error "$1.tex" && \
 #pdflatex -file-line-error -halt-on-error "$1.tex"
